4cb744da2e8477723018817be89868a679cb95c6
ShivaniBurad <43182492+ShivaniBurad@users.noreply.github.com>
2019-04-24 14:36:54 -0400

    Airport Pair test added and AirportPair modification for dafault init

---
+++ src/Tests/AirportPairTest.java
@@ -0,0 +1,49 @@
+package Tests;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.*;
+
+import FlightHopper.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AirportPairTest {
+
+	AirportPair emptyAirport;
+	
+	@Before
+	
+	public void setup() throws Exception{
+		
+		emptyAirport = new AirportPair();
+		
+		emptyAirport.setStartAirport(null);
+		emptyAirport.setEndAirport("PHL");
+		emptyAirport.setFrequency(10);
+		
+	}
+	@Test
+	
+	public void initAirportPairTest() {
+		String startAirport = "PHL";
+		String endAirport = "LAX";
+		int freq = 100;
+		AirportPair airport1 = new AirportPair();
+		
+		assertNull(airport1.getStartAirport());
+		assertNull(airport1.getEndAirport());
+		assertEquals(-1,airport1.getFrequency());
+		
+		airport1.setStartAirport(startAirport);
+		airport1.setEndAirport(endAirport);
+		airport1.setFrequency(freq);
+		
+		assertEquals("PHL",airport1.getStartAirport());
+		assertEquals("LAX", airport1.getEndAirport());
+		assertEquals(100, airport1.getFrequency());
+		
+	}
+}
