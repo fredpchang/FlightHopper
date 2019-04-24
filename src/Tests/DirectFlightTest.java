49670617a5c7c3d2d54f06bada7190e4b3e67962
fredpchang <fredchang931124@gmail.com>
2019-04-24 12:11:46 -0400

    Added tests

--- src/Tests/DirectFlightTest.java
+++ src/Tests/DirectFlightTest.java
@@ -1,5 +1,45 @@
 package Tests;
 
-public class DirectFlightTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+import FlightHopper.DirectFlight;
+
+public class DirectFlightTest {
+	
+	@Test
+	public void checkValidTest() {
+		DirectFlight f1 = new DirectFlight();
+		DirectFlight f2 = new DirectFlight();
+		
+		f1.setStartTime("201905151200");
+		f1.setEndTime("201905151800");
+		f2.setStartTime("201905151500");
+		f2.setEndTime("201905152000");
+	}
+	
+	@Test
+	public void getFlightRankTest() {
+		DirectFlight flight = new DirectFlight();
+		double price = 650; //dollars
+		int duration = 13; //hours
+		double priceWeight = 0.3; //arbitrary weight
+		
+		//rank formula = (priceInDollar * priceWeight + flightDuration*50*(1-priceWeight)) * 100
+		int expRank = (int) (price * priceWeight + duration*50*(1-priceWeight)) * 100;
+		
+		assertEquals(expRank, flight.getRank());
+		
+	}
+	
+	@Test
+	public void isDirectTest() {
+		DirectFlight flight = new DirectFlight();
+		flight.setDirect(true);
+		
+		assertTrue(flight.isDirect());
+	}
+	
 }
--- src/Tests/NonDirectFlightTest.java
+++ src/Tests/NonDirectFlightTest.java
@@ -1,16 +1,16 @@
 package Tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
-import FlightHopper.DirectFlight;
+import FlightHopper.NonDirectFlight;
 public class NonDirectFlightTest {
 
 	@Test
 	public void checkValidTest() {
-		DirectFlight f1 = new DirectFlight();
-		DirectFlight f2 = new DirectFlight();
+		NonDirectFlight f1 = new NonDirectFlight();
+		NonDirectFlight f2 = new NonDirectFlight();
 		
 		f1.setStartTime("201905151200");
 		f1.setEndTime("201905151800");
@@ -20,12 +20,21 @@ public class NonDirectFlightTest {
 	
 	@Test
 	public void getFlightRankTest() {
-		DirectFlight flight = new DirectFlight();
+		NonDirectFlight flight = new NonDirectFlight();
+		double price = 650; //dollars
+		int duration = 13; //hours
+		double priceWeight = 0.3; //arbitrary weight
+		
+		//rank formula = (priceInDollar * priceWeight + flightDuration*50*(1-priceWeight)) * 100
+		int expRank = (int) (price * priceWeight + duration*50*(1-priceWeight)) * 100;
+		
+		assertEquals(expRank, flight.getRank());
+		
 	}
 	
 	@Test
 	public void isDirectTest() {
-		DirectFlight flight = new DirectFlight();
+		NonDirectFlight flight = new NonDirectFlight();
 		flight.setDirect(true);
 		
 		assertTrue(flight.isDirect());
