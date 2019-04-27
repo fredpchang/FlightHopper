563e32cc9664f6c50593f1197da38551513332b4
fredpchang <fredchang931124@gmail.com>
2019-04-27 14:36:26 -0400

    Updates to Parser

--- src/FlightHopper/FlightHopper.java
+++ src/FlightHopper/FlightHopper.java
@@ -1,12 +1,22 @@
 package FlightHopper;
 
+import java.io.IOException;
 import java.util.*;
+
+import org.json.simple.parser.ParseException;
 
 public class FlightHopper {
     public int queryType;
 //    Map<String, Airport> airports = new HashMap<>();
 
     public static void main(String[] args) {
-
+    	FlightScraper fs = new FlightScraper();
+    	
+    	try {
+			fs.jsonParser("files/jsonFlights/phl-sfo-flight-results.json");
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
